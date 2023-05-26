c8bf575feebfbcae1d4a3e660933becaf401d7ae
just1ce415 <dimon.ka2002@gmail.com>
2023-05-27 00:35:35 +0300

    1. Adjusted some numpy matrix operations calls. 2. Added functions docs. 3. Added early exageration. 4. Added default class parameters. 5. t-SNE metrics collecting. 6. Different Initializations. 7. Beautiful plots. 8. Partially corrected experiment functions.

--- t_sne.py
+++ t_sne.py
@@ -1,6 +1,7 @@
 import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 
 #################################################
 # Probabilities
@@ -31,7 +32,7 @@ def p_conditional_probabilities(X, sigmas):
     # for numerical stability
     # smaller numbers won't overfill float
     X_stable = X_norm - np.max(X_norm, axis=1).reshape(-1, 1)
-    X_exp = np.e**X_stable
+    X_exp = np.exp(X_stable)
 
     np.fill_diagonal(X_exp, 0)
 
@@ -160,8 +161,8 @@ def kl_divergence(P, Q):
     return terms.sum()
 
 
-def kl_divergence_grad(P, Q, Y, D_recip):
-    pq_expand = np.expand_dims(P - Q, 2)
+def kl_divergence_grad(P, Q, Y, D_recip, eef=1):
+    pq_expand = np.expand_dims(eef*P - Q, 2)
 
     y_i = np.expand_dims(Y, 1)
     y_j = np.expand_dims(Y, 0)
@@ -170,7 +171,6 @@ def kl_divergence_grad(P, Q, Y, D_recip):
     d_expand = np.expand_dims(D_recip, 2)
 
     grad = 4 * (pq_expand * y_diff * d_expand).sum(axis=1)
-
     return grad
 
 
@@ -189,24 +189,48 @@ def l2_penalty_grad(Y, beta):
 
 
 class TSNE:
+    """
+    Arguments:
+        n_components: number of output dimensions
+        num_iters: number of iterations of optimization
+        perplexity: soft number of neighbors for each point to form a cluster
+        learning_rate: learning rate
+        momentum: momentum term
+        compression_period: number of iterations for early compression
+        compression_term: L2 penalty factor for early compression
+        initialization: type of initialization: 'random' or 'pca'
+        ee: enable early exaggeration
+        ee_iterations: if ee=True, number of iteration for early exaggeration
+        verbose: enable plotting every 100 iterations
+        color: if verbose=True, list ints of size N (number of samples) which represent encoded labels
+        seed: seed for random generator
+    """
     def __init__(
         self,
-        num_iters,
-        perplexity,
-        learning_rate,
-        momentum,
+        n_components=2,
+        num_iters=1000,
+        perplexity=30,
+        learning_rate=200,
+        momentum=0.001,
         compression_period=20,
         compression_term=2,
+        initialization='random',
+        ee=False,
+        ee_iterations=250,
         verbose=False,
         color=None,
         seed=0,
     ):
         self.num_iters = num_iters
+        self.n_components = n_components
         self.perplexity = perplexity
         self.learning_rate = learning_rate
         self.momentum = momentum
         self.compression_period = compression_period
         self.compression_term = compression_term
+        self.initialization = initialization
+        self.ee = ee
+        self.ee_iterations = ee_iterations
 
         self.verbose = verbose
         self.color = color
@@ -214,12 +238,17 @@ class TSNE:
         self.seed = seed
 
         self.sigmas = None
+        self.Y = None
 
-        self.metrics = {"kl_divergence": 0, "spearman_corr": 0}
+        self.metrics = {"kl_divergence": [], "spearman_corr": []}
+
 
     def fit(self, X):
         rng = np.random.default_rng(seed=self.seed)
         N = X.shape[0]
+
+        if self.ee:
+            self.learning_rate = N / 12
 
         neg_sq_distances = -pairwise_sq_distances(X)
 
@@ -228,7 +257,12 @@ class TSNE:
 
         P = p_joint_probabilities(neg_sq_distances, self.sigmas)
 
-        Y = rng.normal(0, 1e-4, (N, 2))
+        if self.Y is None:
+            if self.initialization == 'random':
+                Y = rng.normal(0, 1e-4, (N, self.n_components))
+            elif self.initialization == 'pca':
+                pca = PCA(n_components=self.n_components)
+                Y = pca.fit(X=X)
 
         Y = self._gradient_descent(P, Y)
 
@@ -241,7 +275,10 @@ class TSNE:
         for t in tqdm(range(self.num_iters)):
             Q, D_inv = q_joint_probabilities(Y)
 
-            grad = kl_divergence_grad(P, Q, Y, D_inv)
+            if self.ee and t < self.ee_iterations:
+                grad = kl_divergence_grad(P, Q, Y, D_inv, eef=12)
+            else:
+                grad = kl_divergence_grad(P, Q, Y, D_inv)
             if t < self.compression_period:
                 grad += l2_penalty_grad(Y, self.compression_term)
 
@@ -255,7 +292,7 @@ class TSNE:
                 plt.scatter(Y[:, 0], Y[:, 1], c=self.color)
                 plt.show()
 
-        self.metrics["kl_divergence"] = kl_divergence(P, Q)
+        self.metrics["kl_divergence"].append(kl_divergence(P, Q))
 
         return Y
 
@@ -265,6 +302,14 @@ class TSNE:
     def _learning_rate(self, t):
         return self.learning_rate
 
+    def plot_metrics(self):
+        fig, axs = plt.subplots(1, 1, figsize=(6, 6))
+        axs[0][0] = plt.plot(self.metrics["kl_divergence"])
+        axs[0][0].set_title("KL Divergence")
+        axs[0][0].set_xlabel("Iteration")
+        axs[0][0].set_ylabel("Value")
+        fig.show()
+
 
 #################################################
 # Example
@@ -272,12 +317,7 @@ class TSNE:
 
 
 def run_TSNE():
-    num_iters = 10
-    perplexity = 20
-    lr = 10.0
-    momentum = 0.9
-
-    tsne = TSNE(num_iters, perplexity, lr, momentum)
+    tsne = TSNE(num_iters=10)
 
     X_test = np.array([[1, 2, 3], [1, 1, 1], [3, 3, 3]], dtype="float64")
     Y_test = np.power(X_test, 1e-4)
